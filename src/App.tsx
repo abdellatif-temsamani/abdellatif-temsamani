996253ae13d211e397815ec2d5d3243aeb8769c7
abdellatif-dev <abdellatif.devlog@gmail.com>
2022-05-31 20:42:08 +0100

    feat: Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import { Component } from 'react';
+import { Header } from './Components';
 
-function App() {
-
-    return (
-        <div className="App">
-            <p>Hello Vite + React!</p>
-        </div>
-    )
+class App extends Component {
+    render() {
+        return (
+            <div className="App" >
+                <Header />
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="Header" >
+                <img src="/profile.jpeg" alt="profile" />
+                <div className="notflex">
+                    <h3>Abdellatif Mkared Temsamani</h3>
+                    <p>software engineer</p>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
+
